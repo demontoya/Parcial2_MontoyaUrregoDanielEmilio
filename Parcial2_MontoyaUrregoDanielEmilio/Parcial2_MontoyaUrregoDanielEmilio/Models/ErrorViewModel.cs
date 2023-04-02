2d46930125f2671f584cdef971687eaac56c1a6b
Daniel Montoya <danielemilio201@gmail.com>
2023-04-01 19:02:26 -0500

    Created proyect Parcial2_MontoyaUrregoDanielEmilio

---
+++ Parcial2_MontoyaUrregoDanielEmilio/Parcial2_MontoyaUrregoDanielEmilio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_MontoyaUrregoDanielEmilio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
