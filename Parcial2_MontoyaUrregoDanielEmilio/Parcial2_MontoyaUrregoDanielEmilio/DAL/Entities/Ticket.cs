b45505dd6c82ee8bfe06dd1721e0aa87fb00a922
Daniel Montoya <danielemilio201@gmail.com>
2023-04-02 10:11:46 -0500

    i created controller TicketsController and new changes  in class Ticket.

--- Parcial2_MontoyaUrregoDanielEmilio/Parcial2_MontoyaUrregoDanielEmilio/DAL/Entities/Ticket.cs
+++ Parcial2_MontoyaUrregoDanielEmilio/Parcial2_MontoyaUrregoDanielEmilio/DAL/Entities/Ticket.cs
@@ -7,10 +7,13 @@ namespace Parcial2_MontoyaUrregoDanielEmilio.DAL.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Numero Ticket")]
         public int Id { get; set; }
-
+        [Display(Name = "Fecha de uso")]
         public DateTime? UseDate { get; set; }
+        [Display(Name = "Estado uso")]
         public bool? IsUsed { get; set; }
+        [Display(Name = "Puerta de entrada")]
         public string? EntranceGate { get; set; }
 
     }
