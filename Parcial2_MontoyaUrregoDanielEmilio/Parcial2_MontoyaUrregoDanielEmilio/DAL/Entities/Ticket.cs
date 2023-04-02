e475fd16e5f7fc27dd2623f1e434438be04751d6
Daniel Montoya <danielemilio201@gmail.com>
2023-04-01 19:28:36 -0500

    Created class called Ticket

---
+++ Parcial2_MontoyaUrregoDanielEmilio/Parcial2_MontoyaUrregoDanielEmilio/DAL/Entities/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial2_MontoyaUrregoDanielEmilio.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime? UseDate { get; set; }
+        public bool? IsUsed { get; set; }
+        public string? EntranceGate { get; set; }
+
+    }
+}
